f575a7c3fe7057d46a0096a359ae9ac1a60f2f14
elangtuyur <143047820+elangsurya7095@users.noreply.github.com>
2023-11-13 22:33:35 +0700

    Create registration.php

---
+++ rentalmobil/user/includes/registration.php
@@ -0,0 +1,104 @@
+<?php
+//error_reporting(0);
+session_start();
+include('config.php');
+if(isset($_POST['signup']))
+{
+$fname=$_POST['fullname'];
+$email=$_POST['emailid']; 
+$mobile=$_POST['mobileno'];
+$password=md5($_POST['password']); 
+$sql="INSERT INTO  tblusers(FullName,EmailId,ContactNo,Password) VALUES(:fname,:email,:mobile,:password)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':fname',$fname,PDO::PARAM_STR);
+$query->bindParam(':email',$email,PDO::PARAM_STR);
+$query->bindParam(':mobile',$mobile,PDO::PARAM_STR);
+$query->bindParam(':password',$password,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+echo "<script>alert('Registration successfull. Now you can login');</script>";
+}
+else 
+{
+echo "<script>alert('Something went wrong. Please try again');</script>";
+}
+}
+
+?>
+
+
+<script>
+function checkAvailability() {
+$("#loaderIcon").show();
+jQuery.ajax({
+url: "check_availability.php",
+data:'emailid='+$("#emailid").val(),
+type: "POST",
+success:function(data){
+$("#user-availability-status").html(data);
+$("#loaderIcon").hide();
+},
+error:function (){}
+});
+}
+</script>
+<script type="text/javascript">
+function valid()
+{
+if(document.signup.password.value!= document.signup.confirmpassword.value)
+{
+alert("Password and Confirm Password Field do not match  !!");
+document.signup.confirmpassword.focus();
+return false;
+}
+return true;
+}
+</script>
+<div class="modal fade" id="signupform">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title">Daftar</h3>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="signup_wrap">
+            <div class="col-md-12 col-sm-6">
+              <form  method="post" name="signup" onSubmit="return valid();">
+                <div class="form-group">
+                  <input type="text" class="form-control" name="fullname" placeholder="Nama Panjang" required="required">
+                </div>
+                      <div class="form-group">
+                  <input type="text" class="form-control" name="mobileno" placeholder="Nomor Telepon" maxlength="12" required="required">
+                </div>
+                <div class="form-group">
+                  <input type="email" class="form-control" name="emailid" id="emailid" onBlur="checkAvailability()" placeholder="Alamat Email" required="required">
+                   <span id="user-availability-status" style="font-size:12px;"></span> 
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" name="confirmpassword" placeholder="Konfirmasi Password" required="required">
+                </div>
+                <div class="form-group checkbox">
+                  <input type="checkbox" id="terms_agree" required="required" checked="">
+                  <label for="terms_agree">Saya Setuju <a href="#">Terms and Conditions</a></label>
+                </div>
+                <div class="form-group">
+                  <input type="submit" value="Sign Up" name="signup" id="submit" class="btn btn-block">
+                </div>
+              </form>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer text-center">
+        <p>Sudah Punya Akun? <a href="../index.php">Masuk Disini</a></p>
+      </div>
+    </div>
+  </div>
+</div>
