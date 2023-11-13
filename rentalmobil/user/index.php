ea760792f2de9577a378a9835a83fe6b6e8852e7
elangtuyur <143047820+elangsurya7095@users.noreply.github.com>
2023-11-13 22:33:28 +0700

    Update index.php

--- rentalmobil/user/index.php
+++ rentalmobil/user/index.php
@@ -1,76 +1,64 @@
 <?php
 session_start();
+include('includes/config.php');
+if(isset($_POST['login']))
+{
+$email=$_POST['email'];
+$password=md5($_POST['password']);
+$sql ="SELECT EmailId,Password,FullName FROM tblusers WHERE EmailId=:email and Password=:password";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['login']=$_POST['email'];
+$_SESSION['fname']=$results->FullName;
+$currentpage=$_SERVER['REQUEST_URI'];
+echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
+} else{
+  
+  echo "<script>alert('Invalid Details');</script>";
+
+}
+
+}
 
 ?>
 
-
-
-<!doctype html>
-<html lang="en" class="no-js">
-
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-	<title>Rental | User Login</title>
-	<link rel="stylesheet" href="../admin/css/font-awesome.min.css">
-	<link rel="stylesheet" href="../admin/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../admin/css/dataTables.bootstrap.min.css">
-	<link rel="stylesheet" href="../admin/css/bootstrap-social.css">
-	<link rel="stylesheet" href="../admin/css/bootstrap-select.css">
-	<link rel="stylesheet" href="../admin/css/fileinput.min.css">
-	<link rel="stylesheet" href="../admin/css/awesome-bootstrap-checkbox.css">
-	<link rel="stylesheet" href="../admin/css/style.css">
-</head>
-
-<body>
-	
-	<div class="login-page bk-img" style="background-image: url(../admin/img/login-bg.jpg);">
-		<div class="form-content">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 col-md-offset-3">
-						<h1 class="text-center text-bold mt-4x" style="color:#fff">User | Sign in (proses)</h1>
-						<div class="well row pt-2x pb-3x bk-light">
-							<div class="col-md-8 col-md-offset-2">
-								<form method="post">
-
-									<label for="" class="text-uppercase text-sm">Username </label>
-									<input type="text" placeholder="Username" name="username" class="form-control mb">
-
-									<label for="" class="text-uppercase text-sm">Password</label>
-									<input type="password" placeholder="Password" name="password" class="form-control mb">
-		
-
-									<button class="btn btn-primary btn-block" name="login" type="submit">LOGIN</button>
-
-								</form>
-
-			<p style="margin-top: 4%" align="center"><a href="../index.php">Kembali</a>	</p>
-							</div>
-
-						</div>
-							
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	
-	<!-- Loading Scripts -->
-	<script src="../admin/js/jquery.min.js"></script>
-	<script src="../admin/js/bootstrap-select.min.js"></script>
-	<script src="../admin/js/bootstrap.min.js"></script>
-	<script src="../admin/js/jquery.dataTables.min.js"></script>
-	<script src="../admin/js/dataTables.bootstrap.min.js"></script>
-	<script src="../admin/js/Chart.min.js"></script>
-	<script src="../admin/js/fileinput.js"></script>
-	<script src="../admin/js/chartData.js"></script>
-	<script src="../admin/js/main.js"></script>
-
-</body>
-
-</html>
+<div class="modal fade" id="loginform">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title">Masuk</h3>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="login_wrap">
+            <div class="col-md-12 col-sm-6">
+              <form method="post">
+                <div class="form-group">
+                  <input type="email" class="form-control" name="email" placeholder="Alamat Email*">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" name="password" placeholder="Password*">
+                </div>
+                
+                <div class="form-group">
+                  <input type="submit" name="login" value="Masuk" class="btn btn-block" >
+                </div>
+              </form>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer text-center">
+        <p>Belum Punya Akun? <a href="includes/registration.php">Daftar Disini</a></p>
+        <p><a href="#forgotpassword" data-toggle="modal" data-dismiss="modal">Lupa Password ?</a></p>
+        <p><a href="../index.php">Kembali</a></p>
+      </div>
+    </div>
+  </div>
+</div>
